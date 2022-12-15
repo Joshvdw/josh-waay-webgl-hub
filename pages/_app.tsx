310f31d00afae5bbdba9126c221caff044404973
Josh <Josh@MBP-van-MacBook.hub>
2022-12-15 18:32:09 +1300

    changed lint rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,7 +69,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <a href="https://like-no-ones-watchin.vercel.app/" rel="noreferrer" target={"_blank"}>
             <div className="project">
               <img src="/images/like-no-ones-watchin.png" alt="" />
-              <h2>Like no one&apos;s watchin</h2>
+              <h2>Like no one`&apos;`s watchin</h2>
             </div>
           </a>
           <a href="https://the-tale-of-abe.vercel.app/" rel="noreferrer" target={"_blank"}>
@@ -81,7 +81,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <a href="https://help-me-im-lost.vercel.app/" rel="noreferrer" target={"_blank"}>
             <div className="project">
               <img src="/images/help-me-im-lost.png" alt="" />
-              <h2>Help me! I&apos;m lost!</h2>
+              <h2>Help me! I`&apos;`m lost!</h2>
             </div>
           </a>
         </div>
